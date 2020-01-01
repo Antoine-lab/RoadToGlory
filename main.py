7c1cc47ec335a9e180aff6f15f991846dc75237d
Antoine-lab <53406463+Antoine-lab@users.noreply.github.com>
2020-01-01 20:14:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,20 +42,16 @@ for j in range(i+1,MONSTERS_NUMBERS + i + 1):
     NPC[j].set_sprite('Sprites/004.png')
     NPC[j].set_crop_image(NPC[j].image,(96,0,32,32))
 
-
-
 game = True
 while game:
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
         if event.type == pygame.KEYDOWN:   
             keys = pygame.key.get_pressed()
             if keys[pygame.K_SPACE]:
-                pass
-##                damaged_monsters_id = Player1.attack(Monsters,damaged_monsters_id)
-            if keys[pygame.K_RIGHT]:
+                Player1.attack(NPC)
+            elif keys[pygame.K_RIGHT]:
                 Player1.position = 2
                 Player1.Fx += move
                 if (Player1.get_Frect().collidelist(block)) != -1 or Player1.Fx>(SCREEN_WIDTH-WIDTH):
@@ -83,7 +79,7 @@ while game:
                     Player1.Fy = Player1.posy
                 else:
                     Player1.posy = Player1.Fy
-
+            
             #Putting blocks        
             elif keys[pygame.K_e]:
                 X = Player1.posx//32
@@ -147,28 +143,35 @@ while game:
                             Player1.woods += grid[X][Y+1].materials
                         reSpawnList.append(grid[X][Y+1])
                         reSpawnList[-1].count = -5
-                block = blockInit()
+                
                 
             elif keys[pygame.K_t]:
                 print("Stones : ",Player1.stones)
                 print("Woods : ",Player1.woods)
-
-             
+                
+            
+            
     # Render the map
+##    screen.fill(WHITE)
     mapping(grid,screen)
     # ReSpawn destroyed blocks (method in maps.py)
     reSpawning(reSpawnList)
+    block = blockInit()
     # Update the player
-    Player1.set_crop_image(Player1.image,(96,0,32,32))      
     Player1.animation = (Player1.animation + 1)%3
+    Player1.set_crop_image(Player1.image,(96,0,32,32))
     Player1.update(screen)
+    # Update the NPC
     
-
     for key,value in NPC.items():
+        # Make the mobs move either toward the player if angry or randomly
+        value.move(Player1)
+        # blit and update the mobs
         value.update(screen)
-
+        # Attack the player
+        value.attack(Player1)
     pygame.display.update()
-    clock.tick(FPS)
+    clock.tick(10)
     
 
 
